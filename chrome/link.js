00190f525654b16368d662881160c1b6b52cb7ea
virustotalop <virustotalop@gmail.com>
2016-07-15 17:44:00 -0700

    Updated to 0.0.2, fixed some replacers for the url

--- chrome/link.js
+++ chrome/link.js
@@ -5,6 +5,11 @@ if(hrefStr.includes("out.reddit.com"))
   event.preventDefault();
   hrefStr = hrefStr.split("%2F").join("/");
   hrefStr = hrefStr.split("%3A").join(":");
+  hrefStr = hrefStr.split("%3F").join("?");
+  hrefStr = hrefStr.split("%3D").join("=");
+  hrefStr = hrefStr.split("%26h").join("&h");
+  hrefStr = hrefStr.split("%26w").join("&w");
+  hrefStr = hrefStr.split("%26s").join("&s");
   hrefStr = hrefStr.split("url=")[1];
   hrefStr = hrefStr.split("&token")[0];
   window.open(hrefStr);
